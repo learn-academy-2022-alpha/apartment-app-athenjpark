1e1d81771b6342dbde38a8b73bdef438dedaa29b
Athen Park <athenjpark@gmail.com>
2022-04-13 01:34:49 -0700

    Completed Initial Installs and Resources

---
+++ app/javascript/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
